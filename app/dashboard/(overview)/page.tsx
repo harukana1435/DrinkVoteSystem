c58dfaa93cbaa89fd2d9a2ed1c0b0252367c8de4
harukana1435 <harukana1435@gmail.com>
2024-05-13 19:07:48 +0900

    投票ボタンの配置を変えた

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,24 +2,22 @@ import { lusitana } from '@/app/ui/fonts';
 //import Drink from '@/app/ui/drink_vote/page';
 // import { Vote } from '@/app/ui/drink_vote/page';
 import Drink_info from '@/app/ui/drink_vote/drink_info';
-import { Voting_Button } from '@/app/ui/drink_vote/drink_info';
+import { Vote_Button } from '@/app/ui/drink_vote/button';
 
 export default async function Page() {
   return (
     <main>
-      <div className="relative">
+      <div>
         <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
           飲料投票ページ
         </h1>
-        <div className="p-4 md:h-40">
-          <Drink_info />
-        </div>
-        <div className="bottom--100 absolute right-4">
-          <Voting_Button />
-          {/* <Drink /> */}
-          {/* <Vote /> */}
-        </div>
       </div>
+      <div className="md:h-200 p-4">
+        <Drink_info />
+      </div>
+      <Vote_Button />
+      {/* <Drink /> */}
+      {/* <Vote /> */}
     </main>
   );
 }
