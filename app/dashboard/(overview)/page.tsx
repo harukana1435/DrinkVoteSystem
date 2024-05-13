c1e2eae6a13b2ed5042b9a7579714779833e9ab5
harukana1435 <harukana1435@gmail.com>
2024-05-13 15:48:54 +0900

    add new drink

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,21 +2,24 @@ import { lusitana } from '@/app/ui/fonts';
 //import Drink from '@/app/ui/drink_vote/page';
 // import { Vote } from '@/app/ui/drink_vote/page';
 import Drink_info from '@/app/ui/drink_vote/drink_info';
-
+import { Voting_Button } from '@/app/ui/drink_vote/drink_info';
 
 export default async function Page() {
-
-
-    return (
-        <main>
-            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-                飲料投票ページ
-            </h1>
-            <div className="justify-start p-4 md:h-40">
-                <Drink_info />
-                {/* <Drink /> */}
-                {/* <Vote /> */}
-            </div>
-        </main>
-    );
+  return (
+    <main>
+      <div className="relative">
+        <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+          飲料投票ページ
+        </h1>
+        <div className="p-4 md:h-40">
+          <Drink_info />
+        </div>
+        <div className="bottom--100 absolute right-4">
+          <Voting_Button />
+          {/* <Drink /> */}
+          {/* <Vote /> */}
+        </div>
+      </div>
+    </main>
+  );
 }
