222975caa98eb0dc9ad67feaa58c8399ac5e698e
harukana1435 <harukana1435@gmail.com>
2024-06-06 13:59:58 +0900

    vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,31 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
-
+import Head from 'next/head';
 
 export const metadata: Metadata = {
-    title: {
-        template: '%s | Acme Dashboard',
-        default: 'Acme Dashboard',
-    },
-    description: 'The official Next.js Learn Dashboard built with App Router.',
-    metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-            <body className={`${inter.className} antialiased`}>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <Head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
 }
--- auth.config.ts
+++ auth.config.ts
@@ -6,14 +6,12 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      console.log('a');
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        console.log('b');
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
