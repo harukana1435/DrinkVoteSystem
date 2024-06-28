c1e622681ae77a9ae8710b037d8cebd613e84045
satogo <s2010320@edu.cc.uec.ac.jp>
2024-06-28 19:36:19 +0900

    a

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,44 +5,8 @@ import { fetchTwoteeksResult, addVoteEveryTwoWeeks, deleteVoteEveryTwoWeeks } fr
 import { NextRequest } from 'next/server';
 
 
-export async function EveryTwoWeeks() {
+export async function GET() {
     const VotedList = await fetchTwoteeksResult();
     await addVoteEveryTwoWeeks(); // votedの中身をtotalvotedに追加
     await deleteVoteEveryTwoWeeks(); // votedの中身を削除する
 }
-
-
-// export async function fetchTwoteeksResult() {
-//     noStore();
-//     try {
-//         const data = await sql<DrinkVoted>`SELECT SUM(drink.voted) FROM drink`;
-//         return data.rows;
-//     } catch (error) {
-//         console.error('Database Error:', error);
-//         throw new Error('Failed to fetch two weeks result.');
-//     }
-// }
-
-
-// export async function addVoteEveryTwoWeeks() {
-//     try {
-//         await sql`
-//             UPDATE drink
-//             SET totalvoted = totalvoted + voted
-//         `;
-//     } catch (error) {
-//         return { message: 'Database Error: Failed to add vote every two weeks' };
-//     }
-// }
-
-// export async function deleteVoteEveryTwoWeeks() {
-//     try {
-//         await sql`
-//             UPDATE drink
-//             SET voted = 0
-//         `;
-//     } catch (error) {
-//         return { message: 'Database Error: Failed to delete vote every two weeks' };
-//     }
-// }
-
