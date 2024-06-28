874c9848c18e5cbaa658193dcfe43dbf0717712b
satogo <s2010320@edu.cc.uec.ac.jp>
2024-06-28 15:52:51 +0900

    d

--- app/api/cron/cron.ts
+++ app/api/cron/cron.ts
@@ -1,49 +1,50 @@
-// import { DrinkVoted } from '../../lib/definitions';
-// import { fetchDrink } from '../../lib/data';
-// import { fetchTwoteeksResult, addVoteEveryTwoWeeks, deleteVoteEveryTwoWeeks } from './route';
-
 import { sql } from '@vercel/postgres';
 import { unstable_noStore as noStore } from 'next/cache';
 import { DrinkVoted } from '../../lib/definitions';
+import { fetchTwoteeksResult, addVoteEveryTwoWeeks, deleteVoteEveryTwoWeeks } from '@/app/lib/data';
 
 
-export default async function EveryTwoWeeks() {
+
+
+
+export async function EveryTwoWeeks() {
     const VotedList = await fetchTwoteeksResult();
     await addVoteEveryTwoWeeks(); // votedの中身をtotalvotedに追加
     await deleteVoteEveryTwoWeeks(); // votedの中身を削除
 }
 
-export async function fetchTwoteeksResult() {
-    noStore();
-    try {
-        const data = await sql<DrinkVoted>`SELECT SUM(drink.voted) FROM drink`;
-        return data.rows;
-    } catch (error) {
-        console.error('Database Error:', error);
-        throw new Error('Failed to fetch two weeks result.');
-    }
-}
+
+// export async function fetchTwoteeksResult() {
+//     noStore();
+//     try {
+//         const data = await sql<DrinkVoted>`SELECT SUM(drink.voted) FROM drink`;
+//         return data.rows;
+//     } catch (error) {
+//         console.error('Database Error:', error);
+//         throw new Error('Failed to fetch two weeks result.');
+//     }
+// }
 
 
-export async function addVoteEveryTwoWeeks() {
-    try {
-        await sql`
-            UPDATE drink
-            SET totalvoted = totalvoted + voted
-        `;
-    } catch (error) {
-        return { message: 'Database Error: Failed to add vote every two weeks' };
-    }
-}
+// export async function addVoteEveryTwoWeeks() {
+//     try {
+//         await sql`
+//             UPDATE drink
+//             SET totalvoted = totalvoted + voted
+//         `;
+//     } catch (error) {
+//         return { message: 'Database Error: Failed to add vote every two weeks' };
+//     }
+// }
 
-export async function deleteVoteEveryTwoWeeks() {
-    try {
-        await sql`
-            UPDATE drink
-            SET voted = 0
-        `;
-    } catch (error) {
-        return { message: 'Database Error: Failed to delete vote every two weeks' };
-    }
-}
+// export async function deleteVoteEveryTwoWeeks() {
+//     try {
+//         await sql`
+//             UPDATE drink
+//             SET voted = 0
+//         `;
+//     } catch (error) {
+//         return { message: 'Database Error: Failed to delete vote every two weeks' };
+//     }
+// }
 
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -167,3 +167,37 @@ export async function fetchFilteredDrink(select: string, currentPage: number) {
         throw new Error('Failed to fetch invoices.');
     }
 }
+
+export async function fetchTwoteeksResult() {
+    noStore();
+    try {
+        const data = await sql<DrinkVoted>`SELECT SUM(drink.voted) FROM drink`;
+        return data.rows;
+    } catch (error) {
+        console.error('Database Error:', error);
+        throw new Error('Failed to fetch two weeks result.');
+    }
+}
+
+
+export async function addVoteEveryTwoWeeks() {
+    try {
+        await sql`
+            UPDATE drink
+            SET totalvoted = totalvoted + voted
+        `;
+    } catch (error) {
+        return { message: 'Database Error: Failed to add vote every two weeks' };
+    }
+}
+
+export async function deleteVoteEveryTwoWeeks() {
+    try {
+        await sql`
+            UPDATE drink
+            SET voted = 0
+        `;
+    } catch (error) {
+        return { message: 'Database Error: Failed to delete vote every two weeks' };
+    }
+}
