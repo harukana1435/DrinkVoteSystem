4fcdd105e589238f6648eb6ab4c25da5f20d8c1d
satogo <s2010320@edu.cc.uec.ac.jp>
2024-06-28 15:45:35 +0900

    b

--- app/api/cron/cron.ts
+++ app/api/cron/cron.ts
@@ -2,15 +2,16 @@
 // import { fetchDrink } from '../../lib/data';
 // import { fetchTwoteeksResult, addVoteEveryTwoWeeks, deleteVoteEveryTwoWeeks } from './route';
 
+import { sql } from '@vercel/postgres';
+import { unstable_noStore as noStore } from 'next/cache';
+import { DrinkVoted } from '../../lib/definitions';
+
+
 export default async function EveryTwoWeeks() {
     const VotedList = await fetchTwoteeksResult();
     await addVoteEveryTwoWeeks(); // votedの中身をtotalvotedに追加
     await deleteVoteEveryTwoWeeks(); // votedの中身を削除
 }
-
-import { sql } from '@vercel/postgres';
-import { unstable_noStore as noStore } from 'next/cache';
-import { DrinkVoted } from '../../lib/definitions';
 
 export async function fetchTwoteeksResult() {
     noStore();
