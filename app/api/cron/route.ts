f635afd388e74c675eb5ebb5f12e1a71515cb203
satogo <s2010320@edu.cc.uec.ac.jp>
2024-06-29 01:10:51 +0900

    cron動かす

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,7 +5,7 @@ import { fetchTwoteeksResult, addVoteEveryTwoWeeks, deleteVoteEveryTwoWeeks } fr
 import { NextRequest } from 'next/server';
 
 
-export default async function GET(request: NextRequest) {
+export default async function handlers(request: NextRequest) {
     const VotedList = await fetchTwoteeksResult();
     await addVoteEveryTwoWeeks(); // votedの中身をtotalvotedに追加
     await deleteVoteEveryTwoWeeks(); // votedの中身を削除する
