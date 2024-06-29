ddd8d6039e6c1e45ed23c6cb86e63e39a5fece77
satogo <s2010320@edu.cc.uec.ac.jp>
2024-06-30 02:48:00 +0900

    cron動かす

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -6,8 +6,16 @@ import { addVoteEveryTwoWeeks, deleteVoteEveryTwoWeeks } from '@/app/lib/actions
 import { NextRequest } from 'next/server';
 
 
-export async function GET() {
-    const VotedList = await fetchTwoteeksResult();
-    await addVoteEveryTwoWeeks(); // votedの中身をtotalvotedに追加
-    await deleteVoteEveryTwoWeeks(); // votedの中身を削除する
+export async function GET(request: NextRequest) {
+    try {
+        const VotedList = await fetchTwoteeksResult();
+        await addVoteEveryTwoWeeks(); // votedの中身をtotalvotedに追加
+        await deleteVoteEveryTwoWeeks(); // votedの中身を削除する
+
+        return Response.json({ success: true, message: 'completed cron jobs successfully.' })
+    } catch (error) {
+        console.error('cron jobs error', error);
+        throw new Error('Failed to do cron jobs');
+    }
+
 }
