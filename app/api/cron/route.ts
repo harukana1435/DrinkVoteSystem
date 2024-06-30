2b34bd0d4f438c1bea20778b74c92f33a00d79c3
satogo <s2010320@edu.cc.uec.ac.jp>
2024-06-30 17:45:56 +0900

    cron動かす

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -11,8 +11,8 @@ export async function GET(request: NextRequest) {
     try {
         await addVoteEveryTwoWeeks(); // votedの中身をtotalvotedに追加
         await deleteVoteEveryTwoWeeks(); // votedの中身を削除
-
-        console.log(VotedList)
+        const messages = VotedList.map(item => `${item.name}を${item.price}円分購入します`);
+        console.log(messages)
         return Response.json({ success: true, message: 'completed cron jobs successfully.' })
     } catch (error) {
         console.error('cron jobs error', error);
