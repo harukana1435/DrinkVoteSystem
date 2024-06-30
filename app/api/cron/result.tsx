6b5a002c5b6ec9b7fd55b47d022fb1af99e1fde6
satogo <s2010320@edu.cc.uec.ac.jp>
2024-06-30 23:06:06 +0900

    cron動かす

--- app/api/cron/result.tsx
+++ app/api/cron/result.tsx
@@ -1,13 +1,22 @@
 'use client'
 
+import { useEffect } from 'react';
 import { DrinkResult } from "@/app/lib/definitions"
 
 export default function ShowResult({ results }: { results: DrinkResult[] }) {
     const messages = results.map(result => `${result.name}を${result.price}円分購入します`);
-    alert(messages);
+
+    useEffect(() => {
+        if (messages.length > 0) {
+            alert(messages.join('\n')); // Join messages with newline for better readability
+        }
+    }, [messages]);
+
     return (
         <div>
-            <p>{messages}</p>
+            {messages.map((message, index) => (
+                <p key={index}>{message}</p>
+            ))}
         </div>
     )
 }
