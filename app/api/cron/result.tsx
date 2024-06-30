265e536a98e88fb899287193a16b7e634d37d3e8
satogo <s2010320@edu.cc.uec.ac.jp>
2024-06-30 19:32:02 +0900

    cron動かす

--- app/api/cron/result.tsx
+++ app/api/cron/result.tsx
@@ -4,6 +4,7 @@ import { DrinkResult } from "@/app/lib/definitions"
 
 export function ShowResult({ results }: { results: DrinkResult[] }) {
     const messages = results.map(result => `${result.name}を${result.price}円分購入します`);
+    alert(messages);
     return (
         <div>
             <p>{messages}</p>
