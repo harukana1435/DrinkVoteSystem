c174a47f24d0fe330d536932c4b1366e26c16362
satogo <s2010320@edu.cc.uec.ac.jp>
2024-07-01 00:04:44 +0900

    cron動かす

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,63 +1,21 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { sql } from '@vercel/postgres';
+import { unstable_noStore as noStore } from 'next/cache';
+import { DrinkVoted } from '../../lib/definitions';
 import { fetchTwoteeksResult } from '@/app/lib/data';
 import { addVoteEveryTwoWeeks, deleteVoteEveryTwoWeeks } from '@/app/lib/actions';
+import { NextRequest } from 'next/server';
+import Result from './a';
 
 export async function GET(request: NextRequest) {
     const VotedList = await fetchTwoteeksResult();
-    const messages = VotedList.map(result => `${result.name}を${result.price}円分購入します`);
-
+    const messages = VotedList.map(DrinkResult => `${DrinkResult.name}を${DrinkResult.price}円分購入します`);
     try {
         await addVoteEveryTwoWeeks(); // votedの中身をtotalvotedに追加
         await deleteVoteEveryTwoWeeks(); // votedの中身を削除
-
         console.log(messages);
-
-        // HTMLの生成
-        const html = `
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Cron Job Results</title>
-            </head>
-            <body>
-                <h2>Cron Job Results</h2>
-                <ul>
-                    ${messages.map(message => `<li>${message}</li>`).join('')}
-                </ul>
-            </body>
-            </html>
-        `;
-
-        // HTMLをレスポンスとして返す
-        return new NextResponse(html, {
-            headers: {
-                'Content-Type': 'text/html',
-            },
-        });
+        return Response.json({ success: true, message: 'completed cron jobs successfully.' })
     } catch (error) {
-        console.error('Cron job error', error);
-
-        // エラーレスポンスのHTML生成
-        const errorHtml = `
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Error</title>
-            </head>
-            <body>
-                <h2>Failed to execute cron job</h2>
-            </body>
-            </html>
-        `;
-
-        return new NextResponse(errorHtml, {
-            headers: {
-                'Content-Type': 'text/html',
-            },
-        });
+        console.error('cron jobs error', error);
+        throw new Error('Failed to do cron jobs');
     }
 }
