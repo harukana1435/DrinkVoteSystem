d91a00ff9de5aa429832adfcb53b9900f1caf887
satogo <s2010320@edu.cc.uec.ac.jp>
2024-07-02 13:38:41 +0900

    2週間の結果を表示する

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -188,7 +188,7 @@ export async function fetchLatestResult() {
     try {
         const latestdate = await sql`SELECT date FROM result ORDER BY date DESC LIMIT 1`;
         const latestdatestr = latestdate.rows[0].toString()
-        const drinklist = await sql<DrinkResult>`SELECT result.name, result.price WHERE drink.date = ${latestdatestr}`;
+        const drinklist = await sql<DrinkResult>`SELECT result.name, result.price FROM result WHERE drink.date = ${latestdatestr}`;
         return drinklist.rows
     } catch (error) {
         console.error('Database Error:', error);
