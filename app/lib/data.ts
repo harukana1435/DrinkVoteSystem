1922032a34ab201cc6df7a9c84522ffb7273271a
satogo <s2010320@edu.cc.uec.ac.jp>
2024-07-03 15:11:39 +0900

    前回の結果を値段の降順で表示

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -177,7 +177,6 @@ export async function fetchTwoteeksResult() {
         const value2 = Math.floor(1500 / result2.rows[0].sum) || 0;
         const result3 = await sql<DrinkResult>`SELECT drink.name, drink.japanesename, (${value2} * drink.voted) AS price 
         FROM drink WHERE ${value} * drink.voted > 0
-        ORDER BY drink.voted DESC
         `;
         return result3.rows;
     } catch (error) {
@@ -191,7 +190,10 @@ export async function fetchLatestResult() {
     try {
         const latestdate = await sql`SELECT result.date AS resultdate FROM result ORDER BY result.date DESC LIMIT 1`;
         const latestdatestr = latestdate.rows[0].resultdate.toString();
-        const drinklist = await sql<DrinkResult>`SELECT result.name, result.japanesename, result.price FROM result WHERE result.date = ${latestdatestr}`;
+        const drinklist = await sql<DrinkResult>`SELECT result.name, result.japanesename, result.price 
+        FROM result WHERE result.date = ${latestdatestr}
+        ORDER BY result.price DESC
+        `;
         return drinklist.rows
     } catch (error) {
         console.error('Database Error:', error);
