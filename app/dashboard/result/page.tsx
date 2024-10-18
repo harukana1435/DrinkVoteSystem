5802d30ca23d3c37719fd19dea27b50b5d66498d
harukana1435 <harukana1435@gmail.com>
2024-10-18 15:44:03 +0900

    place-holder消した

--- app/dashboard/result/page.tsx
+++ app/dashboard/result/page.tsx
@@ -1,15 +1,12 @@
-import ResultInfo from "@/app/ui/result/result_info";
+import ResultInfo from '@/app/ui/result/result_info';
 
 import { lusitana } from '@/app/ui/fonts';
 
-export default async function Page() {
-
-    return (
-        <div>
-            <h1 className={`${lusitana.className} bg-neutral-100 flex-1 rounded-lg pb-5 pt-5 mb-4 pl-4 text-xl`}>
-                前回の結果
-            </h1>
-            <ResultInfo />
-        </div>
-    );
+export default function Page() {
+  return (
+    <div>
+      <h1 className={`${lusitana.className} specialTitle`}>前回の結果</h1>
+      <ResultInfo />
+    </div>
+  );
 }
--- app/ui/result/result_info.tsx
+++ app/ui/result/result_info.tsx
@@ -1,29 +1,32 @@
-import ShowResult from "@/app/ui/result/showresult";
-import { fetchLatestResult } from "@/app/lib/data";
+import ShowResult from '@/app/ui/result/showresult';
+import { fetchLatestResult } from '@/app/lib/data';
+import { fredoka } from '../fonts';
 
 export default async function ResultInfo() {
-    const resultlist = await fetchLatestResult();
-    const totalPrice = resultlist.reduce((acc, result) => acc + result.price, 0);
+  const resultlist = await fetchLatestResult();
+  const totalPrice = resultlist.reduce((acc, result) => acc + result.price, 0);
 
-    return (
-        <div>
-            <div className="display: flex flex-wrap gap-10">
-                {resultlist.map((result) => (
-                    <div
-                        key={result.name}
-                        className="mt-3 flex flex-col items-center justify-center"
-                    >
-                        {
-                            <ShowResult
-                                name={result.name}
-                                japanesename={result.japanesename}
-                                price={result.price}
-                            />
-                        }
-                    </div>
-                ))}
-            </div>
-            合計{totalPrice}円
-        </div>
-    );
+  return (
+    <div>
+      <div className="display: flex flex-wrap gap-10">
+        {resultlist.map((result) => (
+          <div
+            key={result.name}
+            className="mt-3 flex flex-col items-center justify-center"
+          >
+            {
+              <ShowResult
+                name={result.name}
+                japanesename={result.japanesename}
+                price={result.price}
+              />
+            }
+          </div>
+        ))}
+      </div>
+      <p className={`ml-2 ${fredoka.className} extrabold text-xl`}>
+        Total Prices: {totalPrice}円分
+      </p>
+    </div>
+  );
 }
